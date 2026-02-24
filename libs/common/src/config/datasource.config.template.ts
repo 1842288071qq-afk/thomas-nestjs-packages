31b6ffd617b0c318e6fda4c7a7002304bc88a65b
LuBaobei <hobairiku@gmail.com>
2026-02-24 13:58:51 +0800

    chore: 调整模块依赖

---
+++ libs/common/src/config/datasource.config.template.ts
@@ -0,0 +1,41 @@
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies/snake-naming.strategy';
+import { DataSourceConfig } from './config.interface';
+
+type DatasourceType =
+  | 'postgres'
+  | 'mysql'
+  | 'sqlite'
+  | 'mariadb'
+  | 'mongodb'
+  | 'oracle'
+  | 'mssql'
+  | 'sqljs'
+  | 'cordova'
+  | 'nativescript'
+  | 'react-native'
+  | 'expo'
+  | 'better-sqlite3';
+
+export const datasourceConfigObject = (
+  type: DatasourceType,
+): Record<string, DataSourceConfig> => {
+  const host = process.env.DATABASE_HOST || 'localhost';
+  const port = parseInt(process.env.DATABASE_PORT || '5432', 10);
+  const username = process.env.DATABASE_USER || 'postgres';
+  const password = process.env.DATABASE_PASSWORD || 'postgres';
+  const database = process.env.DATABASE_NAME || 'postgres';
+  return {
+    default: {
+      type,
+      host,
+      port,
+      username,
+      password,
+      database,
+      autoLoadEntities: true,
+      synchronize: false,
+      logging: process.env.NODE_ENV === 'development' ? true : false,
+      namingStrategy: new SnakeNamingStrategy(),
+    },
+  };
+};
--- libs/common/src/utils/index.ts
+++ libs/common/src/utils/index.ts
@@ -1,6 +1,4 @@
 export * from './dataScopeEngine';
 export * from './id';
-export * from './identity';
 export * from './password';
-export * from './question-bank';
 export * from './tree';
